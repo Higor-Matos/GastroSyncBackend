905734d5e0067de1d025966047d501a9be9d7c47
Higor de Deus <higordeus22@gmail.com>
2023-09-21 11:04:01 -0300

    SQLServer Express instanciado e funcionado.

--- GastroSyncBackend.Domain/Entities/Mesa.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GastroSyncBackend.Domain.Entities
-{
-    public class Mesa
-    {
-        public int Id { get; set; }
-        public int Numero { get; set; }
-        public List<Produto> ListaProdutos { get; set; } = new List<Produto>();
-    }
-}
