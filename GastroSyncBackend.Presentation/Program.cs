cde8c59ace742b57c2240837eb723682d55f8b87
Higor de Deus <higordeus22@gmail.com>
2023-09-21 15:40:17 -0300

    Program refatorada

--- GastroSyncBackend.Presentation/Program.cs
+++ GastroSyncBackend.Presentation/Program.cs
@@ -9,29 +9,7 @@ using NLog.Web;
 var logger = NLog.LogManager.GetCurrentClassLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseNLog();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Configuração do DbContext
-const string connectionString = "Server=sqlserver,1433;Database=GastroSyncDb;User Id=sa;Password=MyPass@word;Encrypt=False;";
-builder.Services.AddDbContext<IAppDbContext, AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-var containerBuilder = new ContainerBuilder();
-
-// Varredura de Assembly e Atributo AutoDI
-containerBuilder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
-    .Where(t => t.GetCustomAttributes(typeof(AutoDIAttribute), false).Any())
-    .AsImplementedInterfaces();
-
-containerBuilder.Populate(builder.Services);
-var container = containerBuilder.Build();
-
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-
-builder.Services.AddAutofac();
+ConfigureApp(builder);
 
 var app = builder.Build();
 
@@ -39,25 +17,14 @@ try
 {
     logger.Info("Configurando e construindo a aplicação...");
 
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<IAppDbContext>();
-
-        // Tente criar o banco de dados, se ele não existir
-        dbContext.EnsureDatabaseCreated();
-        logger.Info("Banco de dados verificado com sucesso.");
-    }
+    InitializeDatabase(app);
 
     if (app.Environment.IsDevelopment())
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        logger.Info("Swagger habilitado no ambiente de desenvolvimento.");
+        EnableSwagger(app);
     }
 
-    app.UseHttpsRedirection();
-    app.UseAuthorization();
-    app.MapControllers();
+    ConfigureAppMiddleware(app);
 
     logger.Info("Aplicação iniciada e pronta para receber requisições...");
 
@@ -73,4 +40,54 @@ catch (Exception ex)
 finally
 {
     NLog.LogManager.Shutdown();
+}
+
+void ConfigureApp(WebApplicationBuilder builder)
+{
+    builder.Host.UseNLog();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var configuration = builder.Configuration;
+
+    // Configuração do DbContext
+    builder.Services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+    var containerBuilder = new ContainerBuilder();
+    ConfigureContainer(containerBuilder, builder.Services);
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+}
+
+void ConfigureContainer(ContainerBuilder containerBuilder, IServiceCollection services)
+{
+    // Varredura de Assembly e Atributo AutoDI
+    containerBuilder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+        .Where(t => t.GetCustomAttributes(typeof(AutoDIAttribute), false).Any())
+        .AsImplementedInterfaces();
+
+    containerBuilder.Populate(services);
+}
+
+void InitializeDatabase(IHost app)
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<IAppDbContext>();
+    dbContext.EnsureDatabaseCreated();
+    logger.Info("Banco de dados verificado com sucesso.");
+}
+
+void EnableSwagger(IApplicationBuilder app)
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    logger.Info("Swagger habilitado no ambiente de desenvolvimento.");
+}
+
+void ConfigureAppMiddleware(WebApplication app)
+{
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
 }
